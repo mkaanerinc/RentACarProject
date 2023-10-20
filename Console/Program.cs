983063976a3ef40f12568314f83e65cf5c7232d4
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2023-10-20 11:21:03 +0300

    Added Car operations with InMemory format.

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+CarManager carManager = new CarManager(new InMemoryCarDal());
+
+carManager.Add(new Car { 
+    CarId = 6, 
+    BrandId = 1, 
+    ColorId = 1, 
+    DailyPrice = 33, 
+    ModelYear = 1998, 
+    Description = "Test Car"
+});
+
+Console.WriteLine(carManager.GetById(6).Description);
+
+carManager.Update( new Car
+{
+    CarId = 6,
+    BrandId = 1,
+    ColorId = 1,
+    DailyPrice = 33,
+    ModelYear = 1998,
+    Description = "Updated Test Car"
+});
+
+Console.WriteLine(carManager.GetById(6).Description);
+
+carManager.Delete(new Car
+{
+    CarId = 6,
+    BrandId = 1,
+    ColorId = 1,
+    DailyPrice = 33,
+    ModelYear = 1998,
+    Description = "Updated Test Car"
+});
+
+List<Car> cars = carManager.GetAll();
+
+foreach (var car in cars)
+{
+    Console.WriteLine(car.Description);
+}
+
