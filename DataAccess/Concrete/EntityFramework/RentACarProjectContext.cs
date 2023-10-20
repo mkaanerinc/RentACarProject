5d403b2c0f27922410e2cc739df6f616e25265bf
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2023-10-20 12:33:52 +0300

    Added IEntityRepository & Added EF Core

---
+++ DataAccess/Concrete/EntityFramework/RentACarProjectContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class RentACarProjectContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-TAKPAA66;Database=RentACarProjectDb;Trusted_Connection=true");
+        }
+    }
+}
