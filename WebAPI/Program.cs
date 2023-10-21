ae8e47757089cec46a8d622065b8f3702a7273aa
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2023-10-21 11:45:23 +0300

    Added Controllers & Added IoC.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,28 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<DbContext, RentACarProjectContext>();
+
+// Business IoC
+builder.Services.AddScoped<IBrandService, BrandManager>();
+builder.Services.AddScoped<ICarService, CarManager>();
+builder.Services.AddScoped<IColorService, ColorManager>();
+builder.Services.AddScoped<ICustomerService, CustomerManager>();
+builder.Services.AddScoped<IRentalService, RentalManager>();
+builder.Services.AddScoped<IUserService, UserManager>();
+
+// DataAccess IoC
+builder.Services.AddScoped<IBrandDal, EFBrandDal>();
+builder.Services.AddScoped<ICarDal, EFCarDal>();
+builder.Services.AddScoped<IColorDal, EFColorDal>();
+builder.Services.AddScoped<ICustomerDal, EFCustomerDal>();
+builder.Services.AddScoped<IRentalDal, EFRentalDal>();
+builder.Services.AddScoped<IUserDal, EFUserDal>();
 
 // Add services to the container.
 
