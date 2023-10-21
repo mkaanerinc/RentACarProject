84284048582202afb64e30f3443967eed4e9d204
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2023-10-21 12:46:45 +0300

    Added Autofac.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,28 +1,20 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<DbContext, RentACarProjectContext>();
-
-// Business IoC
-builder.Services.AddScoped<IBrandService, BrandManager>();
-builder.Services.AddScoped<ICarService, CarManager>();
-builder.Services.AddScoped<IColorService, ColorManager>();
-builder.Services.AddScoped<ICustomerService, CustomerManager>();
-builder.Services.AddScoped<IRentalService, RentalManager>();
-builder.Services.AddScoped<IUserService, UserManager>();
-
-// DataAccess IoC
-builder.Services.AddScoped<IBrandDal, EFBrandDal>();
-builder.Services.AddScoped<ICarDal, EFCarDal>();
-builder.Services.AddScoped<IColorDal, EFColorDal>();
-builder.Services.AddScoped<ICustomerDal, EFCustomerDal>();
-builder.Services.AddScoped<IRentalDal, EFRentalDal>();
-builder.Services.AddScoped<IUserDal, EFUserDal>();
+// Autofac Configuration
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new BusinessModule());
+});
 
 // Add services to the container.
 
