2f167d69490f9be0a26cdcdfb44f6486e3f8d191
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2023-10-21 10:50:22 +0300

    Added CRUD operations for Customer, Rental and User classes.

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        private IRentalDal _rentalDal;
+
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+        public IResult Add(Rental rental)
+        {
+            _rentalDal.Add(rental);
+
+            return new SuccessResult();
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll());
+        }
+
+        public IDataResult<Rental> GetById(int rentalId)
+        {
+            return new SuccessDataResult<Rental>(_rentalDal.Get(c => c.RentalId == rentalId));
+        }
+
+        public IResult Update(Rental rental)
+        {
+            _rentalDal.Update(rental);
+
+            return new SuccessResult();
+        }
+    }
+}
