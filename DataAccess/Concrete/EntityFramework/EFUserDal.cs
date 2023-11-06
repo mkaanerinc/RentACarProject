c6701eb435725b66257a681b404aaf98dd55924e
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2023-11-06 11:43:07 +0300

    Updated IUserDal & EFUserDal to Get Claims.

--- DataAccess/Concrete/EntityFramework/EFUserDal.cs
+++ DataAccess/Concrete/EntityFramework/EFUserDal.cs
@@ -11,5 +11,22 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EFUserDal : EFEntityRepository<User, RentACarProjectContext>, IUserDal
     {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context = new RentACarProjectContext())
+            {
+                var result = (from operationClaim in context.OperationClaims
+                              join userOperationClaim in context.UserOperationClaims
+                              on operationClaim.OperationClaimId equals userOperationClaim.OperationClaimId
+                              where userOperationClaim.UserId == user.UserId
+                              select new OperationClaim
+                              {
+                                  OperationClaimId = operationClaim.OperationClaimId,
+                                  Name = operationClaim.Name
+                              }).ToList();
+
+                return result;
+            }
+        }
     }
 }
